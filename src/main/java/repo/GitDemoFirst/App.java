57fc87914bb991d81f03128b8161d1423e20a28f
mazam78 <maza_05@yahoo.com>
2021-07-21 14:28:55 -0600

    GitDemoFirst

---
+++ src/main/java/repo/GitDemoFirst/App.java
@@ -0,0 +1,7 @@
+package repo.GitDemoFirst;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/repo/GitDemoFirst/AppTest.java
@@ -0,0 +1,19 @@
+package repo.GitDemoFirst;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void postJira() {
+    System.out.println("postJira");
+  }
+  
+  @Test
+  public void deleteTwitter()
+  {
+	  System.out.println("deleteTwitter");
+  }
+}
