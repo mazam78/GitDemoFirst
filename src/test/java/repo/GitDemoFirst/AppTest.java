0cc6d80030b12a283d1a377f7c8f84acc90cac31
mazam78 <maza_05@yahoo.com>
2021-07-21 15:26:45 -0600

    Original user commit

--- src/test/java/repo/GitDemoFirst/AppTest.java
+++ src/test/java/repo/GitDemoFirst/AppTest.java
@@ -12,6 +12,12 @@ public class AppTest {
     System.out.println("second user added work");
     System.out.println("second user added more work");
     System.out.println("second user added more work3");
+    
+    
+    //after pull: first user changes
+    System.out.println("second user added more work4");
+    System.out.println("second user added more work5");
+    System.out.println("second user added more work6");
   }
   
   @Test
---
+++ src/test/java/repo/GitDemoFirst/SeleniumTest.java
@@ -0,0 +1,21 @@
+package repo.GitDemoFirst;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+		
+		System.out.println("WebAutomation");
+	}
+	
+	@Test
+	public void elementsUi() 
+	{
+		System.out.println("elementsUi");
+	}
+
+}
